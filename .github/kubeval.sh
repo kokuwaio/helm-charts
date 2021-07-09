07e6936ec6788f4d0499d7e145695ba8ba0fbde6
André Bauer <monotek@users.noreply.github.com>
2021-07-09 15:33:08 +0200

    Update ci (#85)
    * added contributing & code of conduct
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * update ci
    
    Signed-off-by: André Bauer <andre.bauer@staffbase.com>

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -6,8 +6,7 @@
 set -o errexit
 set -o pipefail
 
-CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
-KUBEVAL_VERSION="0.15.0"
+CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- zammad | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/"
 
 # install kubeval
@@ -20,5 +19,5 @@ for CHART_DIR in ${CHART_DIRS};do
   helm dependency build "${CHART_DIR}"
 
   echo "kubeval(idating) ${CHART_DIR##charts/} chart..."
-  helm template "${CHART_DIR}" | kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
+  helm template "${CHART_DIR}" | kubeval --strict --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
 done
